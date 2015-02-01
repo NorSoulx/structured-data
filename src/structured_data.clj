0cf8dac13894653a7c52e3ef1c4fb8d2c800c323
Christian Røsnes <christian.rosnes@gmail.com>
2015-02-01 15:03:26 +0100

    Exercises 1-9 done

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+  (Math/pow xx xx)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x _ z] v]
+    (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,22 +21,28 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (= (height rectangle) (width rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[px1 py1] point
+        [[rx1 ry1] [rx2 ry2]] rectangle]
+   (and (<= rx1 px1 rx2) (<= ry1 py1 ry2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[rx1 ry1] [rx2 ry2]] inner]
+   (and (contains-point? outer [rx1 ry1]) 
+        (contains-point? outer [rx2 ry2]))))
 
 (defn title-length [book]
   :-)
